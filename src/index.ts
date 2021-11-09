59f790972d8ea4c16f88bb1fdba40c675b266fd5
Anthony Salemo <ajssalemo@gmail.com>
2021-11-09 11:59:08 -0500

    making small changes to test GH Actions build time

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import app from './app';
 
 const port = process.env.PORT || 3033;
 const server = app.listen(port);
+const computer = process.env.COMPUTER_NAME || 'localhost';
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
 server.on('listening', () =>
-  logger.info(`Feathers application started on http://%s:%d - running on ${process.env.COMPUTERNAME}`, app.get('host'), port)
+  logger.info(`Feathers application started on http://%s:%d - running on ${computer}`, app.get('host'), port)
 );
